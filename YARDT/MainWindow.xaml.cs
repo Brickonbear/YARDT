c9798b1ed576f83b6c7e4719f302835a2010d1de
Sebastian <sebthomle@gmail.com>
2020-02-17 21:28:08 +0100

    Removed some uneccecary using's

--- YARDT/MainWindow.xaml.cs
+++ YARDT/MainWindow.xaml.cs
@@ -2,20 +2,12 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace YARDT
